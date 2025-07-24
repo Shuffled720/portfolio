0c81f210eebef06b31f97cefb70df2de2dde1db8
shuffled720 <shuffled720@gmail.com>
2025-07-24 23:42:28 +0530

    adds meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,9 +12,60 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 export const metadata: Metadata = {
-  title: "Vishal's Portfolio",
+  title: "Vishal Kumar | Full-Stack Developer Portfolio",
   description:
-    "Welcome to Vishal's personal portfolio website. Explore projects, skills, and contact information.",
+    "Explore Vishal Kumar's portfolio showcasing web development, full-stack projects, skills, and experience. Built with modern technologies.",
+  keywords: [
+    "Vishal Kumar",
+    "full-stack developer",
+    "portfolio",
+    "projects",
+    "JavaScript",
+    "React",
+    "Next.js",
+    "iit",
+    "web development",
+    "software engineer",
+    "developer portfolio",
+    "iit indore",
+    "full-stack projects",
+    "skills",
+    "experience",
+    "web developer",
+    "software development",
+    "programming",
+    "web applications",
+    "frontend development",
+    "backend development",
+    "UI/UX design",
+  ],
+  authors: [
+    { name: "Vishal Kumar", url: "https://vishalshuffled.vercel.app/" },
+  ],
+  creator: "Vishal Kumar",
+  metadataBase: new URL("https://vishalshuffled.vercel.app/"),
+  openGraph: {
+    title: "Vishal Kumar | Developer Portfolio",
+    description: "Explore my projects, skills, and achievements.",
+    url: "https://vishalshuffled.vercel.app/",
+    siteName: "Vishal's Portfolio",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Vishal Portfolio Banner",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Vishal Kumar | Developer Portfolio",
+    description: "Explore my projects, skills, and achievements.",
+    creator: "@VishalKuma72",
+    images: ["/og-image.png"],
+  },
 };
 
 export default function RootLayout({
